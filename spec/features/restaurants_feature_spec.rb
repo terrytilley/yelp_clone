dbc815d0d1d3960a64f06f48f42329dbf9b6c28a
Aga Kowalczuk <kowalczuk.a@outlook.com>
2016-08-22 16:17:58 +0100

    can add restaurant, save and display, link to restaurant page - displays its name

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -20,4 +20,26 @@ feature 'restaurants' do
       expect(page).not_to have_content('No restaurants yet')
     end
   end
+
+  context 'creating restaurants' do
+    scenario 'prompts user to fill out a form, then displays the new restaurant' do
+      visit '/restaurants'
+      click_link 'Add restaurant'
+      fill_in 'Name', with: 'KFC'
+      click_button 'Create Restaurant'
+      expect(page).to have_content 'KFC'
+      expect(current_path).to eq '/restaurants'
+    end
+  end
+
+  context 'viewing restaurants' do
+    let!(:kfc){Restaurant.create(name: 'KFC')}
+
+    scenario 'lets a user view a restaurant' do
+      visit '/restaurants'
+      click_link 'KFC'
+      expect(page).to have_content 'KFC'
+      expect(current_path).to eq "/restaurants/#{kfc.id}"
+    end
+  end
 end
