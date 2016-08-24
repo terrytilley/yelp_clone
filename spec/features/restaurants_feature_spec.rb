295a162138e953378a93cb822e70cfc6d141225b
Terry <terry@terrytilley.com>
2016-08-24 15:03:20 +0100

    Check if user is logged in before they can edit/delete restaurants

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -22,26 +22,60 @@ feature 'restaurants' do
     end
   end
 
-  context 'creating restaurants' do
-    scenario 'prompts user to fill out a form, then displays the new restaurant' do
-      visit '/restaurants'
-      click_link 'Add a restaurant'
-      fill_in 'Name', with: 'KFC'
-      click_button 'Create Restaurant'
-      expect(page).to have_content('KFC')
-      expect(current_path).to eq('/restaurants')
-    end
-  end
+  context 'when logged in' do
 
-  context 'an invalid restaurant' do
-    it 'does not let you submit a name that is too short' do
-      visit '/restaurants'
-      click_link 'Add a restaurant'
-      fill_in 'Name', with: 'kf'
-      click_button 'Create Restaurant'
-      expect(page).not_to have_css 'h2', text: 'kf'
-      expect(page).to have_content 'error'
+    before :each do
+      sign_up()
     end
+
+    context 'creating restaurants' do
+      scenario 'prompts user to fill out a form, then displays the new restaurant' do
+        visit '/restaurants'
+        click_link 'Add a restaurant'
+        fill_in 'Name', with: 'KFC'
+        click_button 'Create Restaurant'
+        expect(page).to have_content('KFC')
+        expect(current_path).to eq('/restaurants')
+      end
+    end
+
+    context 'an invalid restaurant' do
+      it 'does not let you submit a name that is too short' do
+        visit '/restaurants'
+        click_link 'Add a restaurant'
+        fill_in 'Name', with: 'kf'
+        click_button 'Create Restaurant'
+        expect(page).not_to have_css 'h2', text: 'kf'
+        expect(page).to have_content 'error'
+      end
+    end
+
+    context 'editing restaurants' do
+      before { Restaurant.create name: 'KFC', description: 'Deep fried goodness' }
+
+      scenario 'let a user edit a restaurant' do
+        visit '/restaurants'
+        click_link 'Edit KFC'
+        fill_in 'Name', with: 'Kentucky Fried Chicken'
+        fill_in 'Description', with: "Deep fried goodness"
+        click_button 'Update Restaurant'
+        expect(page).to have_content('Kentucky Fried Chicken')
+        expect(page).to have_content('Deep fried goodness')
+        expect(current_path).to eq('/restaurants')
+      end
+    end
+
+    context 'deleting restaurants' do
+      before { Restaurant.create name: 'KFC', description: 'Deep fried goodness' }
+
+      scenario 'removes a restaurant when a user clicks a delete link' do
+        visit '/restaurants'
+        click_link 'Delete KFC'
+        expect(page).not_to have_content('KFC')
+        expect(page).to have_content('Restaurant deleted successfully')
+      end
+    end
+
   end
 
   context 'viewing restaurants' do
@@ -55,29 +89,11 @@ feature 'restaurants' do
     end
   end
 
-  context 'editing restaurants' do
-    before { Restaurant.create name: 'KFC', description: 'Deep fried goodness' }
-
-    scenario 'let a user edit a restaurant' do
-      visit '/restaurants'
-      click_link 'Edit KFC'
-      fill_in 'Name', with: 'Kentucky Fried Chicken'
-      fill_in 'Description', with: "Deep fried goodness"
-      click_button 'Update Restaurant'
-      expect(page).to have_content('Kentucky Fried Chicken')
-      expect(page).to have_content('Deep fried goodness')
-      expect(current_path).to eq('/restaurants')
-    end
-  end
-
-  context 'deleting restaurants' do
-    before { Restaurant.create name: 'KFC', description: 'Deep fried goodness' }
-
-    scenario 'removes a restaurant when a user clicks a delete link' do
-      visit '/restaurants'
-      click_link 'Delete KFC'
-      expect(page).not_to have_content('KFC')
-      expect(page).to have_content('Restaurant deleted successfully')
+  context 'when not logged in' do
+    scenario 'user cannot add a restaurant' do
+      visit '/'
+      click_link 'Add a restaurant'
+      expect(current_path).to eq "/users/sign_in"
     end
   end
 
---
+++ spec/helpers/sign_up.rb
@@ -0,0 +1,21 @@
+module SignupHelpers
+
+  def sign_up(email: 'test@test.com',
+              password: 'qwerty',
+              password_confirmation: 'qwerty')
+    visit '/users/sign_up'
+    fill_in :user_email, with: email
+    fill_in :user_password, with: password
+    fill_in :user_password_confirmation, with: password_confirmation
+    click_button 'Sign up'
+  end
+
+  def add_restaurant(name: 'KFC', description: 'Deep fried goodness')
+    visit '/restaurants'
+    click_link 'Add restaurant'
+    fill_in :restaurant_name, with: name
+    fill_in :restaurant_description, with: description
+    click_button 'Create Restaurant'
+  end
+  
+end
