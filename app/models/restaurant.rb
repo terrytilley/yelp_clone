899bb5ef14e343f5e721f3be320cf724214a2f58
Terry <terry@terrytilley.com>
2016-08-25 12:18:23 +0100

    Adds review build method in the model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,12 @@
 class Restaurant < ApplicationRecord
-  has_many :reviews, dependent: :destroy
+  
+  has_many :reviews, dependent: :destroy do
+    def build_with_user(attributes = {}, user)
+      attributes[:user] ||= user
+      build(attributes)
+    end
+  end
+
   belongs_to :user
   validates :name, length: { minimum: 3 }, uniqueness: true
 end
